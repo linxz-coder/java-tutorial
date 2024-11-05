45d9c6c6210ec4d8bc6604f6025ce9cdf875297a
linxz-coder <234692261@qq.com>
2024-11-05 23:31:14 +0800

    new

---
+++ chapter02/identifierTest.java
@@ -0,0 +1,33 @@
+/* 
+ # 标识符： 凡是自己可以起名字的地方都叫标识符，比如类名、变量名、方法名、接口名、包名等。
+ 
+ # 标识符命名规则（法律）：
+ > 由26个英文字母大小写，0-9 ，_或 $ 组成  
+> 数字不可以开头。
+> 不可以使用关键字和保留字，但能包含关键字和保留字。
+> Java中严格区分大小写，长度无限制。
+> 标识符不能包含空格。
+
+必须要遵守，否则编译不通过。
+
+# 标识符命名规范（道德）：
+> 包名：多单词组成时所有字母都小写：xxxyyyzzz。
+  例如：java.lang、com.atguigu.bean
+  
+> 类名、接口名：多单词组成时，所有单词的首字母大写：XxxYyyZzz
+  例如：HelloWorld，String，System等
+  
+> 变量名、方法名：多单词组成时，第一个单词首字母小写，第二个单词开始每个单词首字母大写：xxxYyyZzz
+  例如：age,name,bookName,main,binarySearch,getName
+  
+> 常量名：所有字母都大写。多单词时每个单词用下划线连接：XXX_YYY_ZZZ
+  例如：MAX_VALUE,PI,DEFAULT_CAPACITY
+
+  # 拓展阅读：《代码整洁之道》
+ */
+
+class IdentifierTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ chapter02/variableTest.java
@@ -0,0 +1,11 @@
+public class variableTest {
+
+    public static void main(String[] args) {
+        int age = 10; //定义变量+初始化
+
+        char gender; //变量声明
+        gender = '男'; //变量赋值
+
+        System.out.println(age);
+    }
+}
